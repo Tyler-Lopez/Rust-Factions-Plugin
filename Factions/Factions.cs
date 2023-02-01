df5d6f8227c239506025f61954c08e27246f42cd
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-02-01 17:55:28 -0500

    Stub out of project, add Ref

---
+++ Factions/Factions.cs
@@ -0,0 +1,16 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Factions", "Rust Factions", "1.0.0")]
+    [Description("Pending Description")]
+    public partial class Factions : RustPlugin
+    {
+        #region Global Variables
+        private static Factions PluginInstance;
+        #endregion
+
+        public Factions()
+        {
+            PluginInstance = this;
+        }
+    }
+}
