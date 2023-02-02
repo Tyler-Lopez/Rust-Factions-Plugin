af8b45b2cbfdbf1a66d50ba3e7e0d420cfc9645e
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-02-01 19:16:51 -0500

    Project functional build

--- Factions/Build/Factions.cs
+++ Factions/Build/Factions.cs
@@ -13,4 +13,15 @@
             PluginInstance = this;
         }
     }
-}﻿
+}﻿﻿namespace Oxide.Plugins
+{
+    using System;
+    using UnityEngine;
+    partial class Factions
+    {
+        private void OnPlayerConnected(BasePlayer player)
+        {
+            Puts("Player connected!");
+        }
+    }
+}
---
+++ Factions/Src/Hooks/PlayerInput.cs
@@ -0,0 +1,12 @@
+﻿namespace Oxide.Plugins
+{
+    using System;
+    using UnityEngine;
+    partial class Factions
+    {
+        private void OnPlayerConnected(BasePlayer player)
+        {
+            Puts("Player connected!");
+        }
+    }
+}
